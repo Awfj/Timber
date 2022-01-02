f139ca6773269b21879a8de9dbdb512c1bb50ab5
AWFJ <AWFJ@DESKTOP-C0Q2DPJ>
2022-01-02 15:43:21 +0300

    Add project files.

---
+++ Timber.cpp
@@ -0,0 +1,108 @@
+// Timber.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+int main()
+{
+    VideoMode vm(1920, 1080);
+    RenderWindow window(vm, "Timber", Style::Fullscreen);
+
+
+    // BACKGROUND -------------------------------
+    Texture textureBackground;
+    textureBackground.loadFromFile("graphics/background.png");
+
+    Sprite spriteBackground;
+    spriteBackground.setTexture(textureBackground);
+    spriteBackground.setPosition(0, 0);
+
+    // TREE ------------------------------------
+    Texture textureTree;
+    textureTree.loadFromFile("graphics/tree.png");
+    
+    Sprite spriteTree;
+    spriteTree.setTexture(textureTree);
+    spriteTree.setPosition(810, 0);
+
+    // BEE ----------------------------------------
+    Texture textureBee;
+    textureBee.loadFromFile("graphics/bee.png");
+
+    Sprite spriteBee;
+    spriteBee.setTexture(textureBee);
+    spriteBee.setPosition(0, 800);
+
+    bool beeActive = false;
+    float beeSpeed = 0.2f;
+
+    // CLOUDS --------------------------------------
+    Texture textureCloud;
+    textureCloud.loadFromFile("graphics/cloud.png");
+
+    Sprite spriteCloud1;
+    Sprite spriteCloud2;
+    Sprite spriteCloud3;
+
+    spriteCloud1.setTexture(textureCloud);
+    spriteCloud2.setTexture(textureCloud);
+    spriteCloud3.setTexture(textureCloud);
+    
+    spriteCloud1.setPosition(0, 0);
+    spriteCloud2.setPosition(0, 250);
+    spriteCloud3.setPosition(0, 500);
+
+    bool cloud1Active = false;
+    bool cloud2Active = false;
+    bool cloud3Active = false;
+
+    float cloud1Speed = 0.0f;
+    float cloud2Speed = 0.0f;
+    float cloud3Speed = 0.0f;
+
+    Clock clock;
+
+    while (window.isOpen()) {
+        if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+            window.close();
+        }
+
+        window.clear();
+
+        Time dt = clock.restart();
+
+        // BEE -----------------------------------------
+        if (!beeActive) {
+            srand((int)time(0));
+            beeSpeed = (rand() % 200) + 200;
+
+            srand((int)time(0) * 10);
+            float height = (rand() % 500) + 500;
+            spriteBee.setPosition(2000, height);
+            beeActive = true;
+        }
+
+        window.draw(spriteBackground);
+        window.draw(spriteCloud1);
+        window.draw(spriteCloud2);
+        window.draw(spriteCloud3);
+        window.draw(spriteTree);
+        window.draw(spriteBee);
+
+        window.display();
+    }
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
